c288be2449161014134551062228e5e70aac72ea
arden-dave <ardendave-cabotaje@sun-asterisk.com>
2022-09-19 14:45:43 +0800

    integrate frontend and backend of login page
    reabse from create login api branch

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -10,27 +10,50 @@ import Col from "react-bootstrap/Col";
 
 import InputField from "../components/input";
 import { login } from "../store/user";
+import { validate, validateField, getErrorPayload } from "../utils/validation";
+import * as userService from "../services/userService";
 
 const Login = () => {
   const userLogin = useSelector((state) => state.login.value);
   const dispatch = useDispatch();
 
-  const handleSubmit = () => {
-    // do something here
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const errors = validate(userLogin);
+    const payload = { ...userLogin, errors };
+    dispatch(login(payload));
+
+    // if there are no errors, try login through api
+    if (Object.keys(errors).length === 0) {
+      try {
+        await userService.login(userLogin);
+        const payload = { ...userLogin, success: true };
+        dispatch(login(payload));
+      } catch (exception) {
+        if (exception.response) {
+          // if failed, update state and show error
+          const response_error = exception.response.data;
+          const key = Object.keys(response_error);
+          const message = response_error[key];
+          const error = { [key]: message };
+
+          const payload = { ...userLogin, errors: error, success: false };
+          dispatch(login(payload));
+        }
+      }
+    }
   };
 
   const handleChange = ({ currentTarget: input }) => {
     const name = input.id;
     const value = input.value;
-    const error = {};
     let payload = {};
 
-    payload = {
-      ...userLogin,
-      [name]: value,
-      errors: error,
-      success: false,
-    };
+    const errors = validateField(name, value, { ...userLogin });
+
+    payload = getErrorPayload(name, value, errors, { ...userLogin });
+    delete payload.errors.detail;
 
     dispatch(login(payload));
   };
@@ -57,9 +80,14 @@ const Login = () => {
               onChange={handleChange}
               error={userLogin.errors.password}
             />
-            <Button onClick={handleSubmit}>Submit</Button>
+            {userLogin.errors.detail && (
+              <Form.Text muted>{userLogin.errors.detail}</Form.Text>
+            )}
+            {userLogin.success && <Form.Text muted>Login Sucess!</Form.Text>}
+            <div className="d-grid gap-2">
+              <Button onClick={handleSubmit}>Login</Button>
+            </div>
           </Form>
-          {userLogin.success && <Form.Text muted>Login Sucess!</Form.Text>}
           <Link to="/register">Create an account</Link>
         </Col>
       </Row>
