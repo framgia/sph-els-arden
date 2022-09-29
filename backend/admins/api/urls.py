573c587c69e53652f715616655aae7eb370fdd3a
arden-dave <ardendave-cabotaje@sun-asterisk.com>
2022-09-29 13:26:29 +0800

    create questions crud api for admin

--- backend/admins/api/urls.py
+++ backend/admins/api/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import CategoriesTable, SingleCategory
+from .views import CategoriesTable, SingleCategory, CreateQuestionAPI, UpdateQuestionAPI, DeleteQuestionAPI, CategoryQuestions
 
 urlpatterns = [
     path('categories', CategoriesTable.as_view(), name='allCategories'),
     path('category/<int:pk>', SingleCategory.as_view(), name='singleCategory'),
+    path('createQuestion', CreateQuestionAPI.as_view(), name='createQuestion'),
+    path('updateQuestion/<int:pk>', UpdateQuestionAPI.as_view(), name='updateQuestion'),
+    path('deleteQuestion/<int:pk>', DeleteQuestionAPI.as_view(), name='deleteQuestion'),
+    path('categoryQuestions/<int:pk>', CategoryQuestions.as_view(), name='categoryQuestions'),
 ]
