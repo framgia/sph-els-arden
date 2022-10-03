4b151916d5d35aea336d17fae859102623901751
arden-dave <111718037+arden-dave@users.noreply.github.com>
2022-10-03 13:19:29 +0800

    create admin's users view api (#22)
    Co-authored-by: arden-dave <ardendave-cabotaje@sun-asterisk.com>

--- backend/admins/api/urls.py
+++ backend/admins/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import CategoriesTable, SingleCategory, CreateQuestionAPI, ViewUpdateDeleteQuestionAPI, CategoryQuestions
+from .views import CategoriesTable, SingleCategory, CreateQuestionAPI, ViewUpdateDeleteQuestionAPI, CategoryQuestions, ViewUsers
 
 urlpatterns = [
     path('categories', CategoriesTable.as_view()),
@@ -7,4 +7,5 @@ urlpatterns = [
     path('question/<int:pk>', ViewUpdateDeleteQuestionAPI.as_view()),
     path('question', CreateQuestionAPI.as_view()),
     path('category/<int:pk>/questions', CategoryQuestions.as_view()),
+    path('users', ViewUsers.as_view()),
 ]
