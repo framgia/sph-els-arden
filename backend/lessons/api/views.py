62155c3efae1cef1b5be4d0c1507ad63c8ba9dc9
arden-dave <ardendave-cabotaje@sun-asterisk.com>
2022-10-04 11:06:19 +0800

    create api to retrieve questions
    rebase from master

--- backend/lessons/api/views.py
+++ backend/lessons/api/views.py
@@ -1,15 +1,15 @@
 from rest_framework import status
 from rest_framework.views import APIView
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView, ListAPIView
+from rest_framework.generics import ListAPIView
 from rest_framework.response import Response
 from django.db import IntegrityError
 from django.contrib.contenttypes.models import ContentType
 from activities.models import Activity
 
 from lessons.models import Lesson
-from admins.models import Category
+from admins.models import Category, Question
 from .serializers import CreateLessonSerializer
-from admins.api.serializers import CategorySerializer
+from admins.api.serializers import CategorySerializer, QuestionSerializer
 
 class CreateLesson(APIView):
     def post(self, request, pk):
@@ -40,3 +40,12 @@ class CreateLesson(APIView):
 class CategoriesView(ListAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+
+class QuestionsTable(ListAPIView):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
+
+    def get_queryset(self):
+        return self.queryset.filter(
+            category_id=self.kwargs['pk']
+        )
