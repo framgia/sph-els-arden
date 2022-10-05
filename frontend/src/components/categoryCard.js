6c35cfc152c37ec81b25037694e960bf2a62f99f
arden-dave <111718037+arden-dave@users.noreply.github.com>
2022-10-05 18:01:36 +0800

    integrate FE and BE of categories page (#35)
    Co-authored-by: arden-dave <ardendave-cabotaje@sun-asterisk.com>

--- frontend/src/components/categoryCard.js
+++ frontend/src/components/categoryCard.js
@@ -3,18 +3,18 @@ import Card from "react-bootstrap/Card";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
 
-const CategoryCard = () => {
+const CategoryCard = ({ data, onStart }) => {
   return (
     <Card>
-      <Card.Body>
-        <Card.Title>Category title</Card.Title>
-        <Card.Text>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-          quis mi sem. Mauris sit amet libero enim. Maecenas tempor porttitor
-          justo quis blandit.
-        </Card.Text>
+      <Card.Body className="d-flex flex-column">
+        <Card.Title className="text-capitalize">{data.title}</Card.Title>
+        <Card.Text>{data.description}</Card.Text>
         <Col md={{ offset: 6 }} className="d-grid gap-2">
-          <Button fill variant="primary">
+          <Button
+            className="mt-auto align-self-start"
+            variant="primary"
+            onClick={() => onStart(data.id)}
+          >
             Start
           </Button>
         </Col>
--- frontend/src/pages/categories.js
+++ frontend/src/pages/categories.js
@@ -1,19 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import Container from "react-bootstrap/Container";
-import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
+import CardGroup from "react-bootstrap/CardGroup";
 import CategoryCard from "../components/categoryCard";
+import * as lessonService from "../services/lessonService";
 
 const Categories = () => {
+  const [categories, setCategories] = useState();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data } = await lessonService.getCategories();
+      setCategories(data);
+    };
+    fetchData();
+  }, []);
+
+  const handleStart = (id) => {
+    navigate(`/lesson/${id}`);
+  };
+
   return (
     <Container>
       <h1>Categories</h1>
       <Row xs={1} md={4} className="g-4">
-        {Array.from({ length: 9 }).map((_, idx) => (
-          <Col>
-            <CategoryCard />
-          </Col>
-        ))}
+        {categories
+          ? Object.keys(categories).map((key) => (
+              <CardGroup key={key}>
+                <CategoryCard data={categories[key]} onStart={handleStart} />
+              </CardGroup>
+            ))
+          : null}
       </Row>
     </Container>
   );
