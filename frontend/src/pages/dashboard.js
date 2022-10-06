db06bced4b31f8e7873461a70c59bae14c4059ee
arden-dave <111718037+arden-dave@users.noreply.github.com>
2022-10-06 15:29:02 +0800

    design ui for learned words (#38)
    * design ui for learned words
    
    * separated dashboard for admin and normal user
    
    Co-authored-by: arden-dave <ardendave-cabotaje@sun-asterisk.com>

--- frontend/src/pages/dashboard.js
+++ frontend/src/pages/dashboard.js
@@ -1,19 +1,37 @@
 import React, { useContext } from "react";
 import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Image from "react-bootstrap/Image";
 import { UserContext } from "../utils/userContext";
+import WordsPanel from "../components/learnedWordsPanel";
+import avatarPlaceholder from "../asset/avatar_placeholder.png";
+import { Link } from "react-router-dom";
 
 const Dashboard = () => {
   const { user } = useContext(UserContext);
 
   return (
     <Container>
-      {user ? (
-        user.is_staff ? (
-          <h1>Admin Dashboard</h1>
-        ) : (
-          <h1>Dashboard</h1>
-        )
-      ) : null}
+      <h1>Dashboard</h1>
+      <Row>
+        <Col md={3}>
+          <Row>
+            <Col md="auto">
+              <Image src={avatarPlaceholder} style={{ width: "6rem" }} />
+            </Col>
+            <Col md="auto">
+              <p>User Name</p>
+              <Link>Learned 0 words</Link>
+              <br />
+              <Link>Learned 0 lessons</Link>
+            </Col>
+          </Row>
+        </Col>
+        <Col>
+          <WordsPanel />
+        </Col>
+      </Row>
     </Container>
   );
 };
